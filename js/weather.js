e2c9db68be1a59e5cde00bed6533fead2115942a
Breadosaurus <jobwong@ucsc.edu>
2022-11-15 22:01:43 -0800

    Adding Image Buttons

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,35 @@
-function myfun () {
-  document.getElementById("pic").src = "Bloom.png"
+// const img1 = document.createElement("img1");
+var img1 = "img/autumn.png";
+
+// const img2 = document.createElement("img2");
+var img2 = "img/end.jpg";
+
+var img3 = "";
+
+var img4 = "";
+
+var imgArray = [img1, img2, img3, img4];
+
+var i = 0;
+
+function right() {
+  if(i == imgArray.length - 1) { // if we're at the end of the array, go back to the first image
+     i = 0;
+     document.getElementById("pic").src = imgArray[i];
+   }
+   else { // go to the next img
+     i++;
+     document.getElementById("pic").src = imgArray[i];
+   }
+  }
+
+function left() {
+  if(i == 0) { // if we're at the first img, go to the last image
+    i = imgArray.length - 1;
+    document.getElementById("pic").src = imgArray[i];
+  }
+  else { // go to the prev image
+    i--;
+    document.getElementById("pic").src = imgArray[i];
+  }
 }
